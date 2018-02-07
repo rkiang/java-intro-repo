8f38c9156a771b2e9fde3ea49712039a5314bb38
rkiang <ryanlkiang@gmail.com>
2018-02-07 15:45:42 -0600

    added a do-while loop to test loops

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World");
+
+        int thisValue = 5;
+        do { // do-while loops, runs/executes statements until true, and executes the next one after the condition is true
+            System.out.print(thisValue);
+            System.out.print(" * 2 = ");
+            thisValue *= 2;
+            System.out.println(thisValue);
+        } while (thisValue < 100);
     }
 }
